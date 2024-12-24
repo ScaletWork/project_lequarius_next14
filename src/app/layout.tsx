d882d26bd4871a892762e43f87c7aecd2426eed1
ScaletWork <program4@agrotep.com>
2024-12-24 13:26:04 +0200

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import "@/styles/global.scss";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
+export const metadata = {
+  title: "LeQuarius",
+  description: "At LeQuarius, we specialize in delivering innovative IT solutions, including mobile app development, web design, and cloud services, to empower businesses worldwide.",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
